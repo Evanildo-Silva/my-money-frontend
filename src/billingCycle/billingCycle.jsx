b027217efa266789f9ab5264cb819d76f31e5f58
Evanildo Silva <evanildo.sf91@gmail.com>
2023-07-29 11:18:41 -0300

    refactor: billingCycle

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import BillingCycleList from '../billingCycle/billingCycleList'
-import { selectTab, showTabs } from '../common/tab/tabActions'
 import TabContent from '../common/tab/tabContent'
 import TabHeader from '../common/tab/tabHeader'
 import Tabs from '../common/tab/tabs'
@@ -11,13 +10,12 @@ import TabsContent from '../common/tab/tabsContent'
 import TabsHeader from '../common/tab/tabsHeader'
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
-import { create, exclude, update } from './billingCycleActions'
+import { create, exclude, init, update } from './billingCycleActions'
 import BillingCycleForm from './billingCycleForm'
 class BillingCycle extends Component {
 
   componentWillMount() {
-    this.props.selectTab('tabList')
-    this.props.showTabs('tabList', 'tabCreate')
+    this.props.init()
   }
 
   render() {
@@ -67,7 +65,7 @@ class BillingCycle extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ selectTab, showTabs, create, update, exclude }, dispatch)
+  return bindActionCreators({ init, create, update, exclude }, dispatch)
 }
 
 export default connect(null, mapDispatchToProps)(BillingCycle)
